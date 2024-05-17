712c3d62d26039f369322d85b2623b42b0eee559
Gauthier Peyre <gpeyre@c1r3p5.42perpignan.fr>
2024-05-17 15:51:28 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,19 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   magaphone.cpp                                      :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 11:41:38 by gpeyre            #+#    #+#             */
-/*   Updated: 2024/05/17 12:15:23 by gpeyre           ###   ########.fr       */
+/*   Updated: 2024/05/17 15:36:14 by gpeyre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
-int main() 
+int	main(int argc, char **argv) 
 {
-    std::cout << "Bonjour, monde !" << std::endl;
+	std::string sentence;
+	size_t		i;
+
+	i = 0;
+	if (argc != 2)
+	{
+		std::cerr << "Use: " << argv[0] << " <text_to_convert>" << std::endl;
+		return (1);
+	}
+	else
+		sentence = argv[1];
+	while (i < sentence.length())
+	{
+		sentence[i] = std::toupper(sentence[i]);
+		i++;
+	}
+	std::cout << sentence << std::endl;
     return (0);
 }
