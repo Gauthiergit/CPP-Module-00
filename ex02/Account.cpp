befd82ab9bf4420a9fd9504b221cda34ef2f8254
Gauthier Peyre <gpeyre@c3r2p7.42perpignan.fr>
2024-05-24 16:29:24 +0200

    update

---
+++ ex02/Account.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 15:07:30 by gpeyre            #+#    #+#             */
+/*   Updated: 2024/05/24 16:28:48 by gpeyre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include "Account.hpp"
+
+Account::Account( int initial_deposit )
+{
+	
+}
